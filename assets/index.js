001ae22aa4a5dac584fe6858b887a11e199c13fd
lbako801 <lbako98@gmail.com>
2022-12-16 18:53:04 -0700

    added basic navigation functions, js animation

---
+++ assets/index.js
@@ -0,0 +1,20 @@
+// function typeEffect(element, speed) {
+// 	var text = element.innerHTML;
+// 	element.innerHTML = "";
+	
+// 	var i = 0;
+// 	var timer = setInterval(function() {
+//     if (i < text.length) {
+//       element.append(text.charAt(i));
+//       i++;
+//     } else {
+//       clearInterval(timer);
+//     }
+//   }, speed);
+// }
+
+// var speed = 125;
+// var h1 = document.querySelector('h1')
+// var delay = h1.innerHTML.length * speed + speed;
+
+// typeEffect(h1, speed);
