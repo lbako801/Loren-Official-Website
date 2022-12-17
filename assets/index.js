a365d5d2ce0a04776751e456e92023af15d703bb
lbako801 <lbako98@gmail.com>
2022-12-17 00:31:18 -0700

    small CSS margin changes

--- assets/index.js
+++ assets/index.js
@@ -1,20 +1,22 @@
-// function typeEffect(element, speed) {
-// 	var text = element.innerHTML;
-// 	element.innerHTML = "";
+//TYPING ANIMATION FOR TEXT
+
+function typeEffect(element, speed) {
+	var text = element.innerText;
+	element.innerText = "";
 	
-// 	var i = 0;
-// 	var timer = setInterval(function() {
-//     if (i < text.length) {
-//       element.append(text.charAt(i));
-//       i++;
-//     } else {
-//       clearInterval(timer);
-//     }
-//   }, speed);
-// }
+	var i = 0;
+	var timer = setInterval(function() {
+    if (i < text.length) {
+      element.append(text.charAt(i));
+      i++;
+    } else {
+      clearInterval(timer);
+    }
+  }, speed);
+}
 
-// var speed = 125;
-// var h1 = document.querySelector('h1')
-// var delay = h1.innerHTML.length * speed + speed;
+var speed = 40;
+var h1 = document.querySelector('h1')
+var delay = h1.innerHTML.length * speed + speed;
 
-// typeEffect(h1, speed);
+typeEffect(h1, speed);
